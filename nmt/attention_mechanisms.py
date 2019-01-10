051740e9bd01c844dfdcc6c6da42ebf5ef286e34
Rico <rangell@cs.umass.edu>
2019-01-10 16:21:45 -0500

    quick change to attention mechanisms

--- nmt/attention_mechanisms.py
+++ nmt/attention_mechanisms.py
@@ -16,24 +16,13 @@
 
 import tensorflow as tf
 
-from tensorflow.contrib.framework.python.framework import tensor_util
 from tensorflow.python.framework import dtypes
-from tensorflow.python.framework import ops
-from tensorflow.python.framework import tensor_shape
-from tensorflow.python.layers import base as layers_base
 from tensorflow.python.layers import core as layers_core
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import check_ops
-from tensorflow.python.ops import clip_ops
-from tensorflow.python.ops import functional_ops
-from tensorflow.python.ops import init_ops
-from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn_ops
-from tensorflow.python.ops import random_ops
-from tensorflow.python.ops import rnn_cell_impl
-from tensorflow.python.ops import tensor_array_ops
-from tensorflow.python.ops import variable_scope
-from tensorflow.python.util import nest
+
+
+__all__ = ["ReusableLuongAttention", "ReusableBahdanauAttention"]
+
 
 class ReusableLuongAttention(tf.contrib.seq2seq.LuongAttention):
   """Implements Luong-style (multiplicative) attention scoring.
